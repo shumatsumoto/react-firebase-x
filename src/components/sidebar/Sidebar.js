798da73e5c79d5d9e40c75d1a4e99e526932723a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2024-03-19 15:49:05 +0900

    Sidebar作成

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      {/* Twitterアイコン */}
+
+      {/* SidebarOption */}
+
+      {/* ツイートボタン */}
+    </div>
+  )
+}
+
+export default Sidebar
