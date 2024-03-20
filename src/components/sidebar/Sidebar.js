9485668bad37930f38f5dcdb19c8482cc0f17ef0
Shu Matsumoto <shumatsumoto0328@gmail.com>
2024-03-20 13:23:33 +0900

    material Icon

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import XIcon from '@mui/icons-material/X';
+import SidebarOption from './SidebarOption';
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+
 
 const Sidebar = () => {
   return (
@@ -8,6 +18,14 @@ const Sidebar = () => {
       <XIcon className='sidebar__xIcon' />
 
       {/* SidebarOption */}
+      <SidebarOption text="ホーム" Icon={HomeIcon} />
+      <SidebarOption text="話題を検索" Icon={SearchIcon} />
+      <SidebarOption text="通知" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="メッセージ" Icon={MailOutlineIcon} />
+      <SidebarOption text="ブックマーク" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="リスト" Icon={ListAltIcon} />
+      <SidebarOption text="プロフィール" Icon={PermIdentityIcon} />
+      <SidebarOption text="もっとみる" Icon={MoreHorizIcon} />
 
       {/* ツイートボタン */}
     </div>
