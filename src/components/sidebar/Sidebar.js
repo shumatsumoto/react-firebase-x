3ab9123358ea68909e4122da789c4aad1aee7154
Shu Matsumoto <shumatsumoto0328@gmail.com>
2024-03-20 13:11:21 +0900

    materialUI導入

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import XIcon from '@mui/icons-material/X';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       {/* Twitterアイコン */}
+      <XIcon className='sidebar__xIcon' />
 
       {/* SidebarOption */}
 
