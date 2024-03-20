6042bd680c06fb8fd0bfa07c28f189fca4dbc058
Shu Matsumoto <shumatsumoto0328@gmail.com>
2024-03-20 21:50:31 +0900

    Post作成

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import './Timeline.css'
-import TweetBox from './TweetBox'
+import React from "react";
+import "./Timeline.css";
+import TweetBox from "./TweetBox";
+import Post from "./Post";
 
 const Timeline = () => {
   return (
-    <div className='timeline'>
-      <div className='timeline__header'>
+    <div className="timeline">
+      <div className="timeline__header">
         <h2>ホーム</h2>
       </div>
       <TweetBox />
+
+      <Post />
     </div>
+  );
+};
 
-  )
-}
-
-export default Timeline
+export default Timeline;
